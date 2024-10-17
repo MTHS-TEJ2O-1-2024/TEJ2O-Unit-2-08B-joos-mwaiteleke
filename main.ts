16b566c4254a4b4226b941f173b176499700703d
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-10-17 10:35:28 -0400

    Rotates through rgb leds

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Joos
+ * Created on: Oct 2024
+ * This program Rotates through rgb leds
 */
 
-basic.showString('Hello, World!')
+basic.showIcon(IconNames.Happy)
+//Rotates through rgb leds
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+})
+
+basic.showIcon(IconNames.Happy)
