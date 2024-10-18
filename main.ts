d0bfe59ffd645f11ebcb309e0e0f7804a5d8979b
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-10-18 11:19:50 -0400

    added more colours

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
+/*
  * Created by: Joos
  * Created on: Oct 2024
  * This program Rotates through rgb leds
 */
 
 basic.showIcon(IconNames.Happy)
-//Rotates through rgb leds
+//Rotates through pins
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.pause(1000)
@@ -17,6 +16,18 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
 })
-
-basic.showIcon(IconNames.Happy)
