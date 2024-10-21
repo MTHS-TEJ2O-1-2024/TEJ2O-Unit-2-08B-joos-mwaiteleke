dfbe94882b37326310bcb24b69a296070c6cfad3
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-10-21 10:36:59 -0400

    fixed comments

--- main.ts
+++ main.ts
@@ -5,28 +5,40 @@
 */
 
 basic.showIcon(IconNames.Happy)
+
 //Rotates through pins
 input.onButtonPressed(Button.A, function () {
+    // red
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.pause(1000)
+
+    //green
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
+    
+    //blue
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
+    
+    // yellow
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
+    //magenta
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
+    //cyan
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
+    //white
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.pause(1000)
+    //off
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
